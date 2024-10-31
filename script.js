9f92e3091317b849fd51f9288444b2ab09820bfa
André Gabriel Henrique do Nascimento Romano <andreg_h_donascimento@yahoo.com.br>
2024-10-31 19:17:37 -0300

    Improved things

--- script.js
+++ script.js
@@ -1,11 +1,59 @@
-function construcao() {
-    alert('Em construção...');
+const newsletter = document.getElementById('newsletter')
+const fundoalerta = document.getElementById("fundoalerta")
+const enviar = document.getElementById("enviar")
+const aviso = document.querySelector(".aviso")
+const fechar = document.getElementsByClassName("fechar")[0]
+
+enviar.onclick=()=>
+{
+    fundoalerta.style.display = "block"
 }
 
-function enviaremail() {
-    var email = document.getElementById('email');
+fechar.onclick=()=>
+{
+    fundoalerta.style.display = "none"
+}
 
-    if (email.value != "") {
-        alert('E-mail cadastrado!');
+window.onclick=(event)=>
+{
+    if (event.target == fundoalerta)
+    {
+        fundoalerta.style.display = "none"
     }
+}
+
+construcao=()=>
+{
+    fundoalerta.style.display = "block"
+    aviso.innerHTML = 'Em construção...'
+}
+  
+newsletter.addEventListener("submit", (event) =>
+{
+    event.preventDefault()
+    enviaremail()
+})
+
+enviaremail=()=>
+{
+    const email = document.getElementById('email')
+
+    if (email.value === "")
+    {
+        aviso.innerHTML = 'E-mail vazio, preencha.'
+    }
+    else if (!emailvalido(email.value))
+    {
+        aviso.innerHTML = 'E-mail incorreto, corrija.'
+    }
+    else
+    {
+        aviso.innerHTML = 'E-mail cadastrado!'
+        document.getElementById("email").value = ""
+    }
+}
+
+emailvalido=(email)=>
+{
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email)
 }
